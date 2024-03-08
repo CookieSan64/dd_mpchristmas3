b067e7db6da3b09409e354a718400ea6b5907736
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:40:51 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author '! FiftyShadesOfOtaku#0785'
+discord 'https://discord.gg/aUsykSKtEz'
+
+client_script {
+    'new_names.lua',
+  }
+
+  files {
+    'mpchristmas3/mpchristmas3_overlays.xml'
+  }
+
+  data_file 'PED_OVERLAY_FILE' 'mpchristmas3/mpchristmas3_overlays.xml'
